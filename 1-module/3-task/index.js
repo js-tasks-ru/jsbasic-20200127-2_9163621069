f40598bdb2ddc86d4cfe140470ce15d391f49e71
Stanislav Mavlyutov <s.mavlyutov@superjob.ru>
2020-01-25 23:05:01 +0300

    Возвращаем строку где первый элемент в верхнем регистре

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,11 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  if (str === '') {
+    return '';
+  }
+  if (str.length === 1) {
+    return str.charAt(0).toUpperCase();
+  }
+  return str.charAt(0).toUpperCase() + str.substr(1, str.length);
 }
