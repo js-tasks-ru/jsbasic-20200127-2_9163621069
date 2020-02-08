8f4c0521bfb237e3f3e42ddb7c61117f2296e843
Stanislav Mavlyutov <s.mavlyutov@superjob.ru>
2020-02-08 11:23:43 +0300

    Функция getMinMax

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,9 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let result = {};
+  let arr = str.split(/[\s,]+/).filter(Number);
+  result.min = Math.min(...arr);
+  result.max = Math.max(...arr);
+  return result;
 }
