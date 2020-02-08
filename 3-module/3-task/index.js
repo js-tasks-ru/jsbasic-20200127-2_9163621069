6ca438bca22f120422747d7a57801d70812fd873
Stanislav Mavlyutov <s.mavlyutov@superjob.ru>
2020-02-08 21:41:24 +0300

    Функция camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,16 @@
  * @returns {string}
  */
 function camelize(str) {
+  if (str === '') {
+    return '';
+  }
+  let newArr = [];
+  let arr = str.split('-').filter(String);
+  arr.forEach((item) => {
+    newArr += item[0].toUpperCase() + item.substr(1);
+  });
+  if (str[0] !== '-') {
+    newArr = newArr[0].toLowerCase() + newArr.substr(1);
+  }
+  return newArr;
 }
