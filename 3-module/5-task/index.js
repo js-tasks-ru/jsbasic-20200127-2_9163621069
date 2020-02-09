4c0132b69027fcebba8cc1b11e40fdceae41a9fa
Stanislav Mavlyutov <s.mavlyutov@superjob.ru>
2020-02-09 11:53:14 +0300

    Функция filterRange

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,11 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
+  let newArr = [];
+  arr.forEach(function(item) {
+    if (item >= a && item <= b) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 }
