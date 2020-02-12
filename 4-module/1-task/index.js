69c3bce7989f9bae0bf7be10f3cb9b3b283c39ea
Stanislav Mavlyutov <s.mavlyutov@superjob.ru>
2020-02-12 08:44:16 +0300

    Функция makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,9 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let UL = document.createElement('UL');
+  friends.forEach(function(item) {
+    UL.innerHTML += `<li>${item.firstName} ${item.lastName}</li>`;
+  });
+  return UL;
 }
