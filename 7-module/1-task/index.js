8769d0703fbd181ef1474e29f9ae8b0557792eb9
Stanislav Mavlyutov <s.mavlyutov@superjob.ru>
2020-02-26 09:04:47 +0300

    функция promiseClick

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -5,5 +5,10 @@
  */
 
 function promiseClick(button) {
-
+  this.button = button;
+  return new Promise((resolve) => {
+    this.button.addEventListener('click', (event) => {
+      resolve(event);
+    }, { once: true });
+  });
 }
